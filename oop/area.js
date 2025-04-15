ec1693acf515d62af1b02003836766ad964ba544
Naijth <163039373+Naijth@users.noreply.github.com>
2025-04-15 09:56:54 +0200

    div struktura oop

--- oop/area.js
+++ oop/area.js
@@ -1,13 +1,16 @@
-class Area{
-    constructor(className){
-        let containerDiv = document.querySelector('.containeroop');
-        if(!containerDiv){
-            containerDiv = document.createElement('div');
-            containerDiv.className = 'containeroop';
-            document.body.appendChild(containerDiv);
+/**
+ * The class we use to make the container divs
+ */
+class Area{ //creates the area class
+    constructor(className){ //the class's constructor (input is string)
+        let containerDiv = document.querySelector('.containeroop'); //we select the item with the "containeroop" class
+        if(!containerDiv){ //if it doesn't exist
+            containerDiv = document.createElement('div'); //we make a div
+            containerDiv.className = 'containeroop'; //we add the class to it
+            document.body.appendChild(containerDiv); //and append it to the body
         }
-        const div = document.createElement('div');
-        div.className = className;
-        containerDiv.appendChild(div);
+        const div = document.createElement('div'); //we make a div
+        div.className = className; //we set the class to the input
+        containerDiv.appendChild(div); //we append it to the container
     }
 }
